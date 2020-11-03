13921014863cd4c922bed14e469c269ddd21ce20
Birgitta <birgitta.kaplas@kolumbus.fi>
2020-11-03 19:30:04 +0200

    /laskarit/viikko2/Maksukortti/nbproject/ /laskarit/viikko2/Maksukortti/target/ /laskarit/viikko2/Unicafe/target/

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,24 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+    @Test
+    public void saldonLatausToimii() {
+        kortti.lataaRahaa(20);
+        assertEquals(30, kortti.saldo());
+    }
+    @Test
+    public void saldoVaheneeKunRahaRiittaa() {
+        kortti.otaRahaa(4);
+        assertEquals("saldo: 0.6", kortti.toString());
+    }
+    @Test
+    public void saldoEiMuutuKunRahaEiRiita() {
+        kortti.otaRahaa(11);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
 }
