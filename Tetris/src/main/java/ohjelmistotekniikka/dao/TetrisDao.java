66d7ead8145aa6f195ddbecdd02990749ad0aeec
Birgitta <birgitta.kaplas@kolumbus.fi>
2020-12-08 22:03:42 +0200

    close connection

--- Tetris/src/main/java/ohjelmistotekniikka/dao/TetrisDao.java
+++ Tetris/src/main/java/ohjelmistotekniikka/dao/TetrisDao.java
@@ -15,28 +15,34 @@ import java.util.ArrayList;
 public class TetrisDao implements Dao {
     
     private Connection db;
+    private String database;
     
     public TetrisDao(String database) throws SQLException {
+        this.database = database;
         this.db = DriverManager.getConnection("jdbc:sqlite:" + database);
         initializeDatabase();
     }
     
     private void initializeDatabase() throws SQLException {
         Statement s = db.createStatement();
-        s.execute("CREATE TABLE IF NOT EXISTS Points (id INTEGER PRIMARY KEY, name TEXT NOT NULL, points INTEGER)"); 
+        s.execute("CREATE TABLE IF NOT EXISTS Points (id INTEGER PRIMARY KEY, name TEXT NOT NULL, points INTEGER)");
+        db.close();
     }
     
     @Override
     public void addPoints(String name, int points) throws SQLException {
+        db = DriverManager.getConnection("jdbc:sqlite:" + database);
         PreparedStatement p = db.prepareStatement("INSERT INTO Points (name,points) VALUES (?,?)");
         p.setString(1, name);
         p.setInt(2, points);
         
         p.executeUpdate();
+        db.close();
     }
 
     @Override
     public ArrayList<String[]> getTopTree() throws SQLException {
+        db = DriverManager.getConnection("jdbc:sqlite:" + database);
         ArrayList<String[]> results = new ArrayList<>();
         Statement s = db.createStatement();
         ResultSet r = s.executeQuery("SELECT name, points FROM Points ORDER BY points DESC LIMIT 3");
@@ -44,12 +50,13 @@ public class TetrisDao implements Dao {
             String[] row = new String[]{r.getString("name"), Integer.toString(r.getInt("points"))};
             results.add(row);
         }
-        
+        db.close();
         return results;
     }
 
     @Override
     public ArrayList<Integer> getPointsFor(String name) throws SQLException {
+        db = DriverManager.getConnection("jdbc:sqlite:" + database);
         ArrayList<Integer> results = new ArrayList<>();
         PreparedStatement p = db.prepareStatement("SELECT points FROM Points WHERE name = ? ORDER BY points");
         p.setString(1, name);
@@ -57,6 +64,7 @@ public class TetrisDao implements Dao {
         while (r.next()) {
             results.add(r.getInt("points"));
         }
+        db.close();
         return results;
     }
 
